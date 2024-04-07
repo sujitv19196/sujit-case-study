44d224d9798a911855b87044a2bf304c0d1aa798
sujitv19196 <sujit5817@gmail.com>
2024-04-07 08:48:48 -0500

    write api call to gpt backend

--- src/api/api.js
+++ src/api/api.js
@@ -1,19 +1,19 @@
 
 export const getAIMessage = async (userQuery) => {
-  const response = await fetch("http://127.0.0.1:5000/hello", {
-    method: "GET",
+  const response = await fetch("http://127.0.0.1:5000/ask", {
+    method: "POST", 
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*", 
     },
+    body: JSON.stringify({ query: userQuery }), // Send userQuery in the request body
   });
 
   const data = await response.json();
-  const message = 
-    {
-      role: "assistant",
-      content: data.message
-    }
+  const message = {
+    role: "assistant",
+    content: data.message,
+  };
   return message;
 };
 
