b867eac6a2be9e31b007c54590eff508211aba62
sujitv19196 <sujit5817@gmail.com>
2024-04-08 21:18:09 -0500

    update port

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className="heading">
-        Instalily Case Study
+        Ask me about PartSelect.com!
       </div>
         <ChatWindow/>
     </div>
--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 
 export const getAIMessage = async (userQuery) => {
-  const response = await fetch("http://127.0.0.1:5000/ask", {
+  const response = await fetch("http://127.0.0.1:5001/ask", {
     method: "POST", 
     headers: {
       "Content-Type": "application/json",
@@ -16,20 +16,3 @@ export const getAIMessage = async (userQuery) => {
   };
   return message;
 };
-
-// const sendQueryToGPT4 = async (userQuery) => {
-//   const response = await fetch("https://api.openai.com/v1/engines/davinci/completions", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
-//     },
-//     body: JSON.stringify({
-//       prompt: userQuery,
-//       max_tokens: 150,
-//     }),
-//   });
-
-//   const data = await response.json();
-//   return data.choices[0].text;
-// }
